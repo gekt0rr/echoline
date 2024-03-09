d5e4d0c817d6daaf54f6e523904563d3d8c7bf94
gekt0rr <gektor628@gmail.com>
2024-03-09 12:18:36 +0300

    vargrant + playbook

--- shell-script/echoline.sh
+++ shell-script/echoline.sh
@@ -1,7 +1,49 @@
 #!/bin/bash
 
+usage() {
+    echo "Usage: $0 [-t <timer>] [-m <message>] [-h]"
+    echo "Options:"
+    echo "  -t <timer>    Задай временной интервал в секундах (default: 5)"
+    echo "  -m <message>  Задай выводимое сообщение (default: 'HELLO WORLD')"
+    echo "  -h            Показать данную подсказку"
+    exit 1
+}
+
+is_number() {
+    re='^[0-9]+$'
+    if ! [[ $1 =~ $re ]] ; then
+        return 1
+    fi
+    return 0
+}
+
+timer=5
+message="HELLO WORLD"
+
+while getopts ":t:m:h" opt; do
+    case $opt in
+        t)
+            if ! is_number "$OPTARG"; then
+                echo "Не верное значение таймера: $OPTARG. Таймер должен быть числом." >&2
+                exit 1
+            fi
+            timer=$OPTARG
+            ;;
+        m)
+            message=$OPTARG
+            ;;
+        h)
+            usage
+            ;;
+        \?)
+            echo "Неверная опция: -$OPTARG" >&2
+            usage
+            ;;
+    esac
+done
+
 while true; do
     date
-    echo ${2:-"HELLO WORLD"}
-    sleep ${1:-5}
+    echo "$message"
+    sleep "$timer"
 done
