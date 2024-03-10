5f5c13397bed33bf1676fb8d8fb13dbcc9c27153
gekt0rr <gektor628@gmail.com>
2024-03-10 21:11:44 +0300

    readme an comments

--- echoline.sh
+++ echoline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-usage() {
+usage() { # Функция вывода подсказки
     echo "Usage: $0 [-t <timer>] [-m <message>] [-h]"
     echo "Options:"
     echo "  -t <timer>    Задай временной интервал в секундах (default: 5)"
@@ -9,19 +9,19 @@ usage() {
     exit 1
 }
 
-is_number() {
-    re='^[0-9]+$'
-    if ! [[ $1 =~ $re ]] ; then
+is_number() {                       # Функция проверки на число
+    re='^[0-9]+$'                   # Регулярное выражение для числа  
+    if ! [[ $1 =~ $re ]] ; then     # $1 - первый аргумент функции, если он не соответствует регулярному выражению, то вернуть 1 (ошибка)
         return 1
     fi
-    return 0
+    return 0                           
 }
 
-timer=5
-message="HELLO WORLD"
+timer=5                             # Значение по умолчанию для таймера
+message="HELLO WORLD"               # Значение по умолчанию для сообщения
 
-while getopts ":t:m:h" opt; do
-    case $opt in
+while getopts ":t:m:h" opt; do      
+    case $opt in                               
         t)
             if ! is_number "$OPTARG"; then
                 echo "Не верное значение таймера: $OPTARG. Таймер должен быть числом." >&2
